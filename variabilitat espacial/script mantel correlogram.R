719fec5d45632062ed105b1d470a14fd19d41466
Sara Reverte <sara.revertes@gmail.com>
2018-06-26 21:56:08 -0400

    kjj

--- variabilitat espacial/script mantel correlogram.R
+++ variabilitat espacial/script mantel correlogram.R
@@ -1,31 +1,22 @@
 
-setwd("D:/Usuarios/s.reverte/OneDrive - CREAF/TESI/DADES/dades")
-
 library(vegan)
 library(SpatialTools)
-
-b<-read.table("bitxos quantitatiu.txt", header=T)
-database2<-read.table("Database2.txt", header=T)
-
-library(vegan)
 library(betapart)
 
-a<-read.table("flors quantitatiu.txt", header=T)
-b<-read.table("bitxos quantitatiu.txt", header=T)
-c<-read.table("bitxos quantitatiu sense apis.txt", header=T)
-
+database2<-read.table("dades/Database3.txt", header=T)
+a<-read.table("dades/flors quantitatiu.txt", header=T)
+b<-read.table("dades/bitxos quantitatiu.txt", header=T)
+c<-read.table("dades/bitxos quantitatiu sense apis.txt", header=T)
 
 quantitative.plants<-bray.part(a)
 quantitative.polls<-bray.part(b)
 quantitative.senseapis<-bray.part(c)
 
 d <- as.matrix(database2[,7:8])
-c<-dist1(d)
-e<-c*100
-d.dist<-as.dist(e)
+f<-dist1(d)
+d.dist<-as.dist(f*100)
 
 mite.correlog <- mantel.correlog(quantitative.polls$bray, D.geo=d.dist, nperm=999)
-summary(mite.correlog)
 mite.correlog  
 plot(mite.correlog)
 
