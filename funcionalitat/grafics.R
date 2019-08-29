4775604c2305f9d03d8cd6503eba429afec03796
reverteiros <sara.revertes@gmail.com>
2019-08-29 17:08:07 +0200

    dfsdf

--- funcionalitat/grafics.R
+++ funcionalitat/grafics.R
@@ -10,54 +10,42 @@ library(corrplot)
 library(Hmisc)
 
 
-ROF <- meandataperplot %>%
-  filter(Species=="ROF")%>%
-  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate)) %>%
-  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total))
-
-TVUF <- meandataperplot %>%
-  filter(Species=="TVUF")%>%
-  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF)) %>%
-  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total,Fruit_set,Seed_set,Avorted))
-
-Heterospecific <- meandataperplot %>%
-  filter(Species=="TVUF")%>%
-  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF,generality,Proportion_Heterosp_Community)) %>%
-  gather("Fitness_variable","Fitness_value",c(Heterospecific_presence,Proportion_Heterosp_Stigma))
-
-TVUH <- meandataperplot %>%
-  filter(Species=="TVUH")%>%
-  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF)) %>%
-  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total,Fruit_set,Seed_set,Avorted))
-
-ggplot(TVUH, aes(x=Pollinator_value,y=Fitness_value)) +
-  geom_point(alpha=0.3) +
+ggplot(meandataperplot, aes(x=Proportion_used)) +
+  geom_histogram(alpha=0.3) +
   # geom_smooth()+
-  # theme_classic()+
-  facet_grid(Fitness_variable~Pollinator_variable,scales = "free")
-
-
-
-
-
-
+  theme_classic()+
+  facet_grid(.~Species)
 
 
 # barplot visitation rate including taxonomic groups
-datagroups <- read.table("dades/pollinator groups.txt",header=T) %>%
-  select(Plot,Species,Pollinator_group,Abundance)%>%
-  left_join(flowers, by = c("Plot","Species")) %>%
-  filter(.,Species =="ROF" | Species =="TVUF" | Species =="TVUH")%>%
-  mutate(Visitation_rate = Abundance/Flower_Abundance*1000)%>%
-  group_by(Species,Pollinator_group) %>% 
-  summarise(Visitation_rate=mean(Visitation_rate)) %>%
-  filter(.,Pollinator_group != "Mecoptera" & Pollinator_group != "Heteroptera")
+visitationrates <- pollinators %>%
+  select(c(Species,Visitation_rate)) %>%
+  group_by(Species) %>% 
+  summarise(Visitation_rate=mean(Visitation_rate)) 
 
-a <- ggplot(datagroups, aes(fill=Pollinator_group, y=Visitation_rate, x=Species)) + 
+datapollinatorsall2 <- pollinators %>%
+  left_join(networkmetrics, by="Plot") %>%
+  left_join(grupstaxonomicsspread,by=c("Species","Plot")) %>%
+  mutate(HB = Honeybees/Pollinator_abundance) %>%
+  mutate(Bees = Bee/Pollinator_abundance) %>%
+  mutate(Coleopt = Coleoptera/Pollinator_abundance) %>%
+  mutate(Dipter = Diptera/Pollinator_abundance) %>%
+  mutate(Lepidopt = Lepidoptera/Pollinator_abundance) %>%
+  mutate(Wasps = Wasp/Pollinator_abundance) %>%
+  select(c(Species,HB,Bees,Coleopt,Dipter,Lepidopt,Wasps)) %>%
+  filter(Wasps > -1) %>%
+  gather(Pollinator_group,"Proportion",3:8)%>%
+  group_by(Species,Pollinator_group) %>% 
+  summarise(Proportion=mean(Proportion)) %>%
+  left_join(visitationrates,by="Species")%>%
+  mutate(Proportion_rate = Proportion * Visitation_rate)
+
+
+a <- ggplot(datapollinatorsall2, aes(fill=Pollinator_group, y=Proportion_rate, x=Species)) + 
   geom_bar(position="stack", stat="identity")+
   theme_classic()+
   theme(legend.position = "top")+ 
-  scale_fill_manual("legend", values = c("Bee" = "red", "Coleoptera" = "blue", "Diptera" = "black", "Honeybees" = "pink", "Lepidoptera" = "green", "Wasp" = "brown"))
+  scale_fill_manual("legend", values = c("Bees" = "red", "Coleopt" = "blue", "Dipter" = "black", "HB" = "pink", "Lepidopt" = "green", "Wasps" = "brown"))
 
 
 colorthreespecies <- c("blue","green","yellow")
@@ -125,5 +113,37 @@ final <- ggarrange(polls,pollen,seeds,nrow=3)
 final
 
 
+
+
+
+
+
+
+
+ROF <- meandataperplot %>%
+  filter(Species=="ROF")%>%
+  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate)) %>%
+  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total))
+
+TVUF <- meandataperplot %>%
+  filter(Species=="TVUF")%>%
+  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF)) %>%
+  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total,Fruit_set,Seed_set,Avorted))
+
+Heterospecific <- meandataperplot %>%
+  filter(Species=="TVUF")%>%
+  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF,generality,Proportion_Heterosp_Community)) %>%
+  gather("Fitness_variable","Fitness_value",c(Heterospecific_presence,Proportion_Heterosp_Stigma))
+
+TVUH <- meandataperplot %>%
+  filter(Species=="TVUH")%>%
+  gather("Pollinator_variable","Pollinator_value",c(Proportion_HB,Proportion_Bee,Proportion_Diptera,Pollinator_richness,Visitation_rate,ProporcioF)) %>%
+  gather("Fitness_variable","Fitness_value",c(Total_presence,Mean_Total,Fruit_set,Seed_set,Avorted))
+
+ggplot(TVUH, aes(x=Pollinator_value,y=Fitness_value)) +
+  geom_point(alpha=0.3) +
+  # geom_smooth()+
+  # theme_classic()+
+  facet_grid(Fitness_variable~Pollinator_variable,scales = "free")
 
 
